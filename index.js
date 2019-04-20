119689da0ad2ce522cfdaac773d5db8004991d5b
Raul Martinez <raulmartinez@Rauls-MacBook-Pro.local>
2019-04-20 15:09:45 -0400

    heroku test

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const serverless = require('serverless-http');
 const Listing = require('./models/Listing');
 const app = express();
 const router = express.Router();
-
+const port = process.env.PORT || 3000;
 router.get('/api', async (req, res) => {
   const pageNo = Number(req.query.pageNo) || 1;
   const pageSize = Number(req.query.pageSize) || 10;
@@ -22,7 +21,5 @@ router.get('/api', async (req, res) => {
   });
 });
 
-app.use('/.netlify/functions/server', router);
-
-module.exports = app;
-module.exports.handler = serverless(app);
+app.use('/', router);
+app.listen(port);
