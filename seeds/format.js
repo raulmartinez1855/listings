24a58bd3df551e15743cd4e928bdc31f80801f4f
Raul Martinez <raulmartinez@Rauls-MacBook-Pro.local>
2019-05-08 05:38:07 -0400

    Added extra filtering

--- seeds/format.js
+++ seeds/format.js
@@ -24,35 +24,37 @@ const fmtData = async () => {
   const json = await rawData();
   const listings = await json.searchresults.result;
 
-  const matches = await listings.map(obj => {
-    const caseNumber = obj.CASENUMBER.split(' ')[0];
-    const adBody = obj.ADBODY[0].split('<dd>');
-    const defData = adBody.reduce(
-      (acc, s, i) => {
-        const str = s.toLowerCase();
+  const matches = await listings
+    .filter(o => !o.CLASSNAME.match(/Ntc Sale/g))
+    .map(obj => {
+      const caseNumber = obj.CASENUMBER.split(' ')[0];
+      const adBody = obj.ADBODY[0].split('<dd>');
+      console.log(obj.CLASSNAME);
+      const defData = adBody.reduce(
+        (acc, s, i) => {
+          const str = s.toLowerCase();
 
-        if (str.match(versus)) acc.name = csvComplaint(adBody[i + 1]);
+          if (str.match(versus)) acc.name = csvComplaint(adBody[i + 1]);
 
-        if (str.match(lastAddress)) {
-          acc.address = csvComplaint(str);
-        }
+          if (str.match(lastAddress)) {
+            acc.address = csvComplaint(str);
+          }
 
-        if (str.match(address)) {
-          acc.address = csvComplaint(fmtCurrentAddress(str));
-        }
+          if (str.match(address)) {
+            acc.address = csvComplaint(fmtCurrentAddress(str));
+          }
 
-        return acc;
-      },
-      { name: 'N/A', address: 'N/A' }
-    );
-
-    return {
-      caseNumber,
-      dateCreated: `${month}/${day}/${year}`,
-      ...defData,
-      notice: obj.CLASSNAME
-    };
-  });
+          return acc;
+        },
+        { name: 'N/A', address: 'N/A' }
+      );
+      return {
+        caseNumber,
+        dateCreated: `${month}/${day}/${year}`,
+        ...defData,
+        notice: obj.CLASSNAME
+      };
+    });
 
   return matches;
 };
